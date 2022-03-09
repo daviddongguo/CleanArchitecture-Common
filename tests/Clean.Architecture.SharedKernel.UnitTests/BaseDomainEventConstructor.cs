98c6750afb6ab2845acccadcaf7f8de125c8be51
daviddongguo <david.dong.guo@gmail.com>
2022-03-09 16:14:35 -0500

    move project from clean architecure

---
+++ tests/Clean.Architecture.SharedKernel.UnitTests/BaseDomainEventConstructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace Clean.Architecture.SharedKernel.UnitTests;
+[TestFixture]
+public class BaseDomainEventConstructor
+{
+  private BaseDomainEvent? _testEvent;
+
+  [SetUp]
+  public void Init()
+  {
+    _testEvent = new Mock<BaseDomainEvent>().Object;
+  }
+
+  [TestCase()]
+  public void InitializesDataOccurred()
+  {
+    Assert.IsNotNull(_testEvent?.DateOccurred);
+    Assert.IsInstanceOf<DateTime>(_testEvent?.DateOccurred);
+    Assert.That(_testEvent?.DateOccurred, Is.LessThan(DateTime.UtcNow));
+  }
+}
---
+++ tests/Clean.Architecture.SharedKernel.UnitTests/BaseEntityConstructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Clean.Architecture.SharedKernel;
+using Moq;
+using NUnit.Framework;
+
+namespace Clean.Architecture.SharedKernel.UnitTests;
+
+[TestFixture]
+public class BaseEntityConstructor
+{
+  private BaseEntity? _testEntity;
+
+  [SetUp]
+  public void Init()
+  {
+    _testEntity = new Mock<BaseEntity>().Object;
+  }
+
+  [TestCase()]
+  public void InitializesId()
+  {
+    Assert.NotNull(_testEntity?.Id);
+    Assert.True(Guid.TryParse(_testEntity?.Id.ToString(), out _));
+  }
+  [TestCase()]
+  public void InitializesEventsToEmptyList()
+  {
+    Assert.That(_testEntity?.Events, Is.Not.Null);
+    Assert.That(_testEntity?.Events, Is.Empty);
+  }
+
+}
